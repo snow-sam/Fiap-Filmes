33aec1faaddda91f9ef39a9a5d2f05ec37949a8a
Samuel Souza <samuel_f2@icloud.com>
2022-09-15 15:35:21 -0300

    Inicio de projeto

---
+++ script.js
@@ -0,0 +1,84 @@
+const api_key = '5109d5dd41ba04b8961cee31002d879e'
+const language = 'pt-br'
+const url_base = 'https://api.themoviedb.org/3'
+const url_base_img = 'https://www.themoviedb.org/t/p/w220_and_h330_face/'
+
+let dados;
+let filmes = document.querySelector("#filmes")
+let series = document.querySelector("#series")
+
+function Card(filme, n_filme) {
+    let card = document.createElement("div")
+    card.classList.add("card")
+    
+    let a = document.createElement("a")
+    a.href = `/detalhes.html?id=${filme.id}&type=${n_filme ? 1 : 2}`
+
+    let poster = document.createElement("img")
+    poster.src = url_base_img + filme.poster_path
+    poster.loading = 'lazy'
+    poster.href = `/detalhes.html?id=${filme.id}`
+    if (n_filme)
+        poster.alt = "Poster do filme " + filme.title
+    else
+        poster.alt = "Poster do filme " + filme.name
+    
+    
+    let titulo = document.createElement("p")
+    if (n_filme)
+        titulo.innerText = filme.title
+    else
+        titulo.innerText = filme.name
+
+    let divNota = document.createElement("div")
+    divNota.classList.add("nota")
+    
+    let estrela = document.createElement("span")
+    estrela.classList.add("material-icons")
+    estrela.classList.add("estrela")
+    estrela.innerText = "star"
+    
+    let nota = document.createElement("span")
+    nota.innerText = filme.vote_average
+    
+    divNota.appendChild(nota)
+    divNota.appendChild(estrela)
+    
+    let botao = document.createElement("a")
+    botao.classList.add("botao")
+    botao.href = `/detalhes.html?id=${filme.id}`
+    botao.innerText = "Detalhes"
+    
+    a.appendChild(poster)
+    card.appendChild(a)
+    card.appendChild(titulo)
+    card.appendChild(divNota)
+    card.appendChild(botao)
+
+    return card
+}
+
+function getMovies(){
+    const movies = fetch(`${url_base}/movie/popular?api_key=${api_key}`)
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(filme => {
+            filmes.appendChild(Card(filme, true))
+        });
+    })
+}
+
+
+function getSeries(){
+    const tv_shows = fetch(`${url_base}/tv/popular?api_key=${api_key}`)
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(serie => {
+            series.appendChild(Card(serie, false))
+        });
+    })
+}
+
+getMovies()
+getSeries()
+
---
+++ script2.js
@@ -0,0 +1,51 @@
+const api_key = '5109d5dd41ba04b8961cee31002d879e'
+const language = 'pt-br'
+const url_base = 'https://api.themoviedb.org/3'
+const url_base_img = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/'
+
+let dados;
+let divSinopse = document.querySelector("#divSinopse")
+let lMenu = document.querySelector("#lMenu")
+
+function Sinopse(filme, n_filme) {
+    let sinopse = document.createElement("div")
+    sinopse.classList.add("sinopse")
+    
+    let poster = document.createElement("img")
+    poster.src = url_base_img + filme.poster_path 
+    poster.alt = `Poster do filme ${filme.title || filme.name}`
+    
+    
+    let titulo = document.createElement("p")
+    titulo.innerText = filme.title || filme.name
+
+    let overview = document.createElement("h6")
+    overview.innerText = filme.overview
+    
+    sinopse.appendChild(titulo)
+    sinopse.appendChild(overview)
+    
+    
+    divSinopse.appendChild(poster)
+    divSinopse.appendChild(sinopse)
+    return
+}
+
+function getSinopse(id){
+    const movies = fetch(`${url_base}/movie/${id}?api_key=${api_key}`)
+    .then(response => response.json())
+    .then(data => Sinopse(data, true))
+}
+
+function getSinopseS(id){
+    const tv = fetch(`${url_base}/tv/${id}?api_key=${api_key}`)
+    .then(response => response.json())
+    .then(data => Sinopse(data, true))
+}
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+metodo = urlParams.get('type') == 1 ? getSinopse : getSinopseS 
+metodo(urlParams.get('id'))
+
+
